cd072ad2bace1969b5cef380dada779cb1d81890
dev-dikirim-in <dev.dikirim.in@gmail.com>
2021-10-10 14:10:19 +0700

    rewrite route login

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ["randomuser.me"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/login",
+        destination: "/auth/login",
+      },
+    ];
+  },
 };
