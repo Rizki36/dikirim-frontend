f58a4476abafb5e2d9dd0e5bf14f04b7a3237a61
Rizki36 <cahrizki.rf@gmail.com>
2021-11-08 12:43:30 +0700

    add login with github

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,22 +6,25 @@ import Header from "../components/Header";
 import { Provider } from "react-redux";
 import store, { persistor } from "@/configs/redux/store";
 import { PersistGate } from "redux-persist/lib/integration/react";
+import { SessionProvider } from "next-auth/react";
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
-          <Head>
-            <title>Dikirim ❤️</title>
-          </Head>
-          <div className="grid grid-cols-[1fr] grid-rows-[auto,1fr]  lg:grid-cols-[auto,1fr] lg:grid-rows-[auto,1fr] h-screen">
-            <Sidebar className="hidden lg:grid row-span-2" />
-            <Header className="" />
-            <div className="pt-6 px-6">
-              <Component {...pageProps} />
+          <SessionProvider session={session} refetchInterval={5 * 60}>
+            <Head>
+              <title>Dikirim ❤️</title>
+            </Head>
+            <div className="grid grid-cols-[1fr] grid-rows-[auto,1fr]  lg:grid-cols-[auto,1fr] lg:grid-rows-[auto,1fr] h-screen">
+              <Sidebar className="hidden lg:grid row-span-2" />
+              <Header className="" />
+              <div className="pt-6 px-6">
+                <Component {...pageProps} />
+              </div>
             </div>
-          </div>
+          </SessionProvider>
         </PersistGate>
       </Provider>
     </>
--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -1,9 +1,24 @@
-const order = () => {
-    return (
-        <div>
-            Order
-        </div>
-    );
-}
+import { useSession, signIn, signOut } from "next-auth/react";
 
-export default order;
+const Order = () => {
+  const { data: session, status } = useSession();
+  const loading = status === "loading";
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div>Order</div>
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  );
+};
+
+export default Order;
